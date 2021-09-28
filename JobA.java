2b0b9a224d867a1bfa32aef1a4897ce600f1694c
iritikjain <85997443+iritikjain@users.noreply.github.com>
2021-09-28 14:48:19 +0530

    2nd Commit

---
+++ JobA.java
@@ -0,0 +1,5 @@
+class JobA{
+    public static void main(String args[]){
+        System.out.println("This Is Job A");
+    }
+}
---
+++ JobB.java
@@ -0,0 +1,5 @@
+class JobB{
+    public static void main(String args[]){
+        System.out.println("This Is Job B");
+    }
+}
